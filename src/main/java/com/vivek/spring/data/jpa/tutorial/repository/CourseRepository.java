e268734b9918b11746f4ac9604d3c1b66a156259
Vivek <Viveksingh6262@gmail.com>
2022-07-16 22:41:36 +0530

    Initial commit

---
+++ src/main/java/com/vivek/spring/data/jpa/tutorial/repository/CourseRepository.java
@@ -0,0 +1,19 @@
+package com.vivek.spring.data.jpa.tutorial.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vivek.spring.data.jpa.tutorial.entity.Course;
+
+import java.util.List;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    Page<Course> findByTitleContaining(
+            String title,
+            Pageable pageable);
+}
